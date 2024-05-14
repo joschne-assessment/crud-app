fd8940120aafe37bda26a35e7f971e89481f0708
joschne <jonas.schneider@balcab.ch>
2024-05-14 22:17:01 +0200

    use git sha as image tag

--- build-and-push-docker-img.sh
+++
@@ -1,6 +0,0 @@
-./mvnw quarkus:build  \
-            -Dquarkus.container-image.push=true \
-            -Dquarkus.container-image.registry="ghcr.io" \
-            -Dquarkus.container-image.group="joschne-assessment" \
-            -Dquarkus.container-image.name="crud-app" \
-            -Dquarkus.container-image.tag="test"
